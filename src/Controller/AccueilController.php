b8e2167a97054fbdc3aafa6952be9c7d7be7bf9b
calexand <alexandre.cmercado@ynov.com>
2025-11-17 21:03:52 -0500

    Création du fichier html.twig

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController
+class AccueilController extends AbstractController
 {
 
     #[Route('/', name: 'accueil')]
     public function index(): Response
     {
-        return new Response('hello world !');
+        return $this->render("pages/accueil.html.twig");
     }
 }
